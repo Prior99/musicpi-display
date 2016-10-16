d6cd502c2524327f1e6d5af9ca606dc9d1c202fc
Frederick <frederick@gnodtke.net>
2016-10-16 20:22:25 +0200

    Added fonts width dimensions 3x5, 5x7 and 7x12.

---
+++ generate-fonts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+aseprite -b ase/7_12.ase --sheet font/7_12.png --sheet-type rows --sheet-width 112 > /dev/null
+aseprite -b ase/5_7.ase --sheet font/5_7.png --sheet-type rows --sheet-width 80 > /dev/null
+aseprite -b ase/3_5.ase --sheet font/3_5.png --sheet-type rows --sheet-width 48 > /dev/null
