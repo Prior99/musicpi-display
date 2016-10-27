d445b44e5cbbc6ec3d7e9e5a536e9a5f1fea8bdf
Frederick <frederick@gnodtke.net>
2016-10-27 23:26:40 +0200

    Added neat animations of seconds on time scene.

--- src/graphics/scene/time.rs
+++ src/graphics/scene/time.rs
@@ -6,6 +6,7 @@ use graphics::RenderInfo;
 use spectrum::SpectrumResult;
 use graphics::scene::Scene;
 use graphics::font::FontRenderer;
+use chrono::Timelike;
 
 pub struct SceneTime {
     font_7x12: FontRenderer
@@ -24,6 +25,28 @@ impl Scene for SceneTime {
     fn draw(&mut self, renderer: &mut Renderer, info: &RenderInfo, _: &SpectrumResult) -> Result<(), String> {
         let hours = info.time.format("%H").to_string();
         let minutes = info.time.format("%M").to_string();
+        let second = info.time.second() as f32;
+        let bar1_width: i32 = (15.0 * ((second -  0.0).max(0.0) / 15.0).min(1.0)) as i32;
+        let bar2_width: i32 = (15.0 * ((second - 15.0).max(0.0) / 15.0).min(1.0)) as i32;
+        let bar3_width: i32 = (15.0 * ((second - 30.0).max(0.0) / 15.0).min(1.0)) as i32;
+        let bar4_width: i32 = (15.0 * ((second - 45.0).max(0.0) / 15.0).min(1.0)) as i32;
+        let mut points = (0 .. bar1_width).map(|x| {
+            let y = 14 + x % 2;
+            Point::new(x, y)
+        }).collect::<Vec<Point>>();
+        points.extend((0 .. bar2_width).map(|x| {
+            let y = x % 2;
+            Point::new(17 + x, y)
+        }));
+        points.extend((0 .. bar3_width).map(|x| {
+            let y = 14 + (x + 1) % 2;
+            Point::new(x, y)
+        }));
+        points.extend((0 .. bar4_width).map(|x| {
+            let y = (x + 1) % 2;
+            Point::new(17 + x, y)
+        }));
+        try!(renderer.draw_points(&points));
         try!(self.font_7x12.text(Point::new(0, 0), &hours, renderer));
         self.font_7x12.text(Point::new(17, 4), &minutes, renderer)
     }
