17862792f36cd8b7fbd0b9742915fa8d8248d133
Frederick <frederick@gnodtke.net>
2016-11-03 18:55:08 +0100

    Fix display problem with spectrum.

--- src/graphics/scene/spectrum.rs
+++ src/graphics/scene/spectrum.rs
@@ -15,7 +15,12 @@ impl SceneSpectrum {
 impl Scene for SceneSpectrum {
     fn draw(&mut self, renderer: &mut Renderer, _: &RenderInfo, spectrum: &SpectrumResult) -> Result<(), String> {
         let mut rects = spectrum.spectrum.iter().enumerate().map(|(x, value)| {
-            let height = value.min(1.0) * 15.0;
+            let fixed_value = if value.is_nan() {
+                0.0f32
+            } else {
+                value.min(1.0)
+            };
+            let height = fixed_value * 16.0;
             Rect::new(x as i32, 16 - height as i32, 1, height as u32)
         }).collect::<Vec<Rect>>();
         rects.push(Rect::new(0, 15, 33, 1));
