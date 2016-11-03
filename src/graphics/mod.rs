88f749c02dcd1d3471422e94d2e5a8a209f12361
Frederick <frederick@gnodtke.net>
2016-11-03 11:24:02 +0100

    Animations now working. Performance problems persist.

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -43,19 +43,6 @@ impl SceneContainer {
     }
 }
 
-fn derasterize_pixels(renderer: &Renderer) -> Result<Vec<Vector2<f32>>, String> {
-    let pixels = try!(renderer.read_pixels(None, PixelFormatEnum::RGBA8888));
-    let mut result: Vec<Vector2<f32>> = Vec::new();
-    for x in 0 .. 32 {
-        for y in 0 .. 16 {
-            let index = (x + y * 32) * 4;
-            if pixels[index] == 255 {
-                result.push(Vector2::new(x as f32, y as f32));
-            }
-        }
-    }
-    Ok(result)
-}
 
 trait ToSdlPoint {
     fn to_sdl(&self) -> Point;
@@ -71,7 +58,6 @@ fn create_transition(origin: Vec<Vector2<f32>>, target: Vec<Vector2<f32>>) -> Ve
     let mut leftover_origins = origin.clone();
     let mut result: Vec<(Vector2<f32>, Vector2<f32>)> = Vec::new();
     for target_point in &target {
-        let point = target_point.clone();
         let min = (&origin).iter().min_by(|a, b| {
             let dist_a = (*a - target_point).norm();
             let dist_b = (*b - target_point).norm();
@@ -128,6 +114,20 @@ impl Graphics {
         }
     }
 
+    fn derasterize_pixels(renderer: &Renderer) -> Result<Vec<Vector2<f32>>, String> {
+        let pixels = try!(renderer.read_pixels(None, PixelFormatEnum::RGBA8888));
+        let mut result: Vec<Vector2<f32>> = Vec::new();
+        for x in 0 .. 32 {
+            for y in 0 .. 16 {
+                let index = (x + y * 32) * 4;
+                if pixels[index] == 255 {
+                    result.push(Vector2::new(x as f32, y as f32));
+                }
+            }
+        }
+        Ok(result)
+    }
+
     fn approach(a: f32, b: f32) -> f32 {
         if a == b {
             a
@@ -145,56 +145,66 @@ impl Graphics {
             return;
         }
         let transition = self.transition.clone().unwrap();
-        self.transition = Some(transition.iter().map(|&(origin, target)| {
+        let mut changed = false;
+        let new_transition = transition.iter().map(|&(origin, target)| {
             if origin == target {
                 (origin, target)
             } else {
                 let x = Graphics::approach(origin.x, target.x);
                 let y = Graphics::approach(origin.y, target.y);
+                if x != origin.x || y != origin.y {
+                    changed = true;
+                }
                 (Vector2::new(x, y), target)
             }
-        }).collect::<Vec<(Vector2<f32>, Vector2<f32>)>>());
+        }).collect::<Vec<(Vector2<f32>, Vector2<f32>)>>();
+        self.transition = if changed {
+            Some(new_transition)
+        } else {
+            None
+        };
     }
 
-    /*fn draw_transition(&mut self, renderer: &mut Renderer) -> Result<(), String> {
+    fn draw_transition(&mut self, renderer: &mut Renderer) -> Result<(), String> {
         renderer.set_draw_color(Color::RGBA(255, 255, 255, 0));
         renderer.clear();
-        if self.transition.is_none() {
-            self.scenes.pop().unwrap();
-            self.scenes.pop().unwrap();
-            let mut scene2 = self.scenes.pop().unwrap();
-            let mut scene1 = self.scenes.pop().unwrap();
-            try!(renderer.render_target().unwrap().set(scene1.texture));
-            renderer.clear();
-            try!(scene1.scene.draw(renderer, &info, &spectrum));
-            let pixels1 = derasterize_pixels(&renderer).unwrap();
-            let origin_texture = renderer.render_target().unwrap().set(scene2.texture).unwrap().unwrap();
-            renderer.clear();
-            try!(scene2.scene.draw(renderer, &info, &spectrum));
-            let pixels2 = derasterize_pixels(&renderer).unwrap();
-            self.transition = Some(create_transition(pixels1, pixels2));
-            renderer.render_target().unwrap().reset();
-            renderer.copy(&origin_texture, None, None);
-        } else {
-            let origins = self.transition.clone()
-                .unwrap()
-                .iter()
-                .map(|&(origin, _)| origin.to_sdl())
-                .collect::<Vec<Point>>();
-            renderer.render_target().unwrap().reset();
-            renderer.set_draw_color(Color::RGBA(0, 0, 0, 255));
-            renderer.draw_points(&origins);
+        let origins = self.transition.clone()
+            .unwrap()
+            .iter()
+            .map(|&(origin, _)| origin.to_sdl())
+            .collect::<Vec<Point>>();
+        try!(renderer.render_target().unwrap().reset());
+        renderer.set_draw_color(Color::RGBA(0, 0, 0, 255));
+        try!(renderer.draw_points(&origins));
+        if self.time % 2 == 0 {
             self.perform_transition();
         }
         Ok(())
+    }
+
+    /*fn start_transition(&mut self, renderer: &mut Renderer) -> Result<(), String> {
+    
     }*/
 
-    fn next_scene(&mut self, info: &RenderInfo) {
-        // Return old scene into front of queue
-        if self.current_scene.is_some() {
+    fn get_pixels_of_scene(container: SceneContainer, renderer: &mut Renderer) ->
+            Result<(SceneContainer, Vec<Vector2<f32>>), String> {
+        try!(renderer.render_target().unwrap().set(container.texture));
+        let pixels = try!(Graphics::derasterize_pixels(renderer));
+        let new_texture = renderer.render_target().unwrap().reset().unwrap().unwrap();
+        Ok((SceneContainer::new(container.scene, new_texture, container.condition), pixels))
+    }
+
+    fn next_scene(&mut self, renderer: &mut Renderer, info: &RenderInfo) -> Result<(), String> {
+        // Return old scene into front of queue and grep derasterized pixels of it
+        let old_pixels = if self.current_scene.is_some() {
             let scene = replace(&mut self.current_scene, None);
-            self.scenes.insert(0, scene.unwrap());
-        }
+            let (swapped_scene, pixels) = Graphics::get_pixels_of_scene(scene.unwrap(), renderer)
+                .expect("Unabled to read pixels from scene.");
+            self.scenes.insert(0, swapped_scene);
+            pixels
+        } else {
+            Vec::new()
+        };
         // Take buffers from top and return them to front if condition not matching
         // until a scene with a matching condition was found
         let mut container = self.scenes.pop().unwrap();
@@ -202,13 +212,18 @@ impl Graphics {
             self.scenes.insert(0, container);
             container = self.scenes.pop().unwrap();
         }
+        // Grab derasterized pixels of new scene
+        let (swapped_container, new_pixels) = Graphics::get_pixels_of_scene(container, renderer)
+            .expect("Error when reading pixels from scene.");
         // Store that one as current scene
-        self.current_scene = Some(container);
+        self.transition = Some(create_transition(old_pixels, new_pixels));
+        self.current_scene = Some(swapped_container);
+        Ok(())
     }
 
-    fn take_current_scene(&mut self, info: &RenderInfo) -> Option<SceneContainer> {
+    fn take_current_scene(&mut self, renderer: &mut Renderer, info: &RenderInfo) -> Option<SceneContainer> {
         if self.current_scene.is_none() {
-            self.next_scene(info);
+            self.next_scene(renderer, info).expect("Could not switch to next scene.");
         }
         replace(&mut self.current_scene, None)
     }
@@ -218,7 +233,7 @@ impl Graphics {
     }
 
     fn draw_scene(&mut self, renderer: &mut Renderer, info: RenderInfo, spectrum: SpectrumResult) -> Result<(), String> {
-        let mut container = self.take_current_scene(&info).unwrap();
+        let mut container = self.take_current_scene(renderer, &info).unwrap();
         renderer.set_draw_color(Color::RGBA(255, 255, 255, 0));
         // Clear window texture
         renderer.clear();
@@ -239,13 +254,12 @@ impl Graphics {
     pub fn draw(&mut self, renderer: &mut Renderer, info: RenderInfo, spectrum: SpectrumResult) -> Result<(), String> {
         // Switch scene after timeout
         if info.ms % SCENE_TIME < self.time % SCENE_TIME {
-            self.next_scene(&info);
+            try!(self.next_scene(renderer, &info));
         }
         self.time = info.ms;
         // Render transition if transition is in progress and else render scene
         if self.transition.is_some() {
-            //self.draw_transition(renderer)
-            self.draw_scene(renderer, info, spectrum)
+            self.draw_transition(renderer)
         } else {
             self.draw_scene(renderer, info, spectrum)
         }
